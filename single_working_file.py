776999fc7acd56d12a0e9f93018ef17825034336
Maurizio PARTON <parton@unich.it>
2023-07-16 02:14:35 +0200

    try

--- single_working_file.py
+++ single_working_file.py
@@ -147,7 +147,24 @@ class EvalCallback(BaseCallback):
     def _on_step(self) -> bool:
         if self.n_calls % self.eval_freq == 0:
             print(self.n_calls)
-            # mean_reward, std_reward = self.evaluate_final_state(self.model, self.eval_env, deterministic=True, n_eval_episodes=1)
+            obs = self.eval_env.reset()
+            print(obs)
+            done = False
+            episode_rewards = 0.0
+
+            while not done:
+              action, _ = model.predict(obs, deterministic=deterministic)
+              print(action)
+              obs, reward, done, info = self.eval_env.step(action)
+              print(reward)
+              #episode_rewards += reward
+
+              #rewards.append(episode_rewards)
+              #env.render()
+
+          #mean_reward = np.mean(rewards)
+          #std_reward = np.std(rewards)
+              # mean_reward, std_reward = self.evaluate_final_state(self.model, self.eval_env, deterministic=True, n_eval_episodes=1)
             # print(f"Mean reward: {mean_reward} at step {self.n_calls}")
 
         return True
