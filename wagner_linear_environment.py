fee1392be9fe84da412c9f4784cf62788d61c1c3
Andrea Fois <andreafois96@gmail.com>
2023-11-17 09:52:03 +0100

    commit

--- wagner_linear_environment.py
+++ wagner_linear_environment.py
@@ -8,17 +8,20 @@ class WagnerLinearEnvironment(gym.Env):
     ACTION_REMOVE = 0 #remove the edge
     ACTION_INSERT = 1 #add the edge
     
-    def __init__(self, number_of_nodes, normalize_reward=True, render_mode="none"):
+    def __init__(self, number_of_nodes):
         super(WagnerLinearEnvironment, self).__init__()
         assert isinstance(number_of_nodes, int), "'number_of_nodes' is not an integer"
         
-        self.render_mode = render_mode
-        self.action_space = spaces.Discrete(2)
         self.number_of_nodes = number_of_nodes
+        
         self.number_of_edges = number_of_nodes * (number_of_nodes -1)//2
+        self.action_space = spaces.Discrete(2)
         self.observation_space = spaces.MultiBinary(2*self.number_of_edges)
-        self.normalize_reward = normalize_reward #Normalize rewards when training, not when evaluating
-        self.reset() #here we set the properties timestep and graph and state
+        self.episode_number = 0
+        self.total_steps = 0
+        self.best_wagner_score = -np.Inf
+        
+        self.reset() # here we set the properties timestep and graph
     
     def compute_nx_graph(self):
         nxgraph = nx.empty_graph(self.number_of_nodes)
@@ -34,53 +37,62 @@ class WagnerLinearEnvironment(gym.Env):
         super().reset(seed=seed, options=options)
         
         self.done = False
+        self.episode_number = self.episode_number + 1
         self.timestep = np.zeros(self.number_of_edges, dtype=np.int8)
         self.timestep[0] = 1
         self.graph = np.zeros(self.number_of_edges, dtype=np.int8)
         self.nxgraph = self.compute_nx_graph()
         self.last_reward = 0
+        self.last_wagner_score = 0
+        self.best_wagner_score_in_episode = -np.Inf
         
         info = {}
         return np.concatenate((self.graph,self.timestep)), info
     
     def step(self, action):
         edge_index = np.argmax(self.timestep)
-        self.graph[edge_index] = action
+        self.graph[edge_index] = action        
+        self.nxgraph = self.compute_nx_graph()
         
         if edge_index < self.number_of_edges - 1:
             self.timestep[edge_index] = 0
             self.timestep[edge_index + 1] = 1
-            self.last_reward = 0.0
         else:
             self.timestep[edge_index] = 0 # The terminal state has timestep part all zero
             self.done = True
-            self.nxgraph = self.compute_nx_graph()
-            if not nx.is_connected(self.nxgraph):
-                self.last_reward = -self.number_of_edges*2 if self.normalize_reward else -5.0 # penalty to be used when the conjecture holds only for connected graphs. this normalization assumes that other rewards are > -1
-            else:
-                constant = 1 + np.sqrt(self.number_of_nodes - 1)
-                lambda_1 = max(np.real(nx.adjacency_spectrum(self.nxgraph)))
-                mu = len(nx.max_weight_matching(self.nxgraph,maxcardinality=True))
-                self.last_reward = constant - (lambda_1 + mu)
         
-        info = {}
+        if not nx.is_connected(self.nxgraph):
+            self.last_wagner_score = -self.number_of_nodes*2 
+        else:
+            constant = 1 + np.sqrt(len(self.nxgraph.nodes) - 1)
+            lambda_1 = max(np.real(nx.adjacency_spectrum(self.nxgraph)))
+            mu = len(nx.max_weight_matching(self.nxgraph,maxcardinality=True))
+            self.last_wagner_score = constant - (lambda_1 + mu)
+        
+        if self.last_wagner_score > self.best_wagner_score_in_episode:
+            self.best_wagner_score_in_episode = self.last_wagner_score
+        if self.last_wagner_score > self.best_wagner_score:
+            self.best_wagner_score = self.last_wagner_score
+        if self.last_wagner_score > 1e-12:
+            self.timestep[edge_index] = 0
+            self.done = True
+            self.render()
+
+        self.total_steps = self.total_steps + 1
+        self.last_reward = 0
         if self.done:
-            print(self.last_reward)
+            self.last_reward = self.last_wagner_score
+            print(f"episode:{self.episode_number}, total_steps:{self.total_steps}, best_score:{self.best_wagner_score}, best_wagner_score_in_episode:{self.best_wagner_score_in_episode}, last_score:{self.last_wagner_score}")
             if self.render_mode == "human":
                 self.render()
+        
+        info = {}
         return np.concatenate((self.graph, self.timestep)), self.last_reward, self.done, False, info
         
     def render(self):
         _, ax = plt.subplots()
         pos = nx.spring_layout(self.nxgraph)
-        title_string = f"wagner1 score = {self.last_reward}"
+        title_string = f"wagner1 score = {self.last_wagner_score}"
         ax.set_title(title_string)
         nx.draw(self.nxgraph, pos=pos, ax=ax, with_labels=True, node_color='lightyellow', font_color='black', edgecolors='black')
-        plt.show()
-        
-        
-        
-        
-        
-    
-    
+        plt.show()    
--- wagner_tests.py
+++ wagner_tests.py
@@ -1,18 +1,22 @@
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, A2C, DQN
+from stable_baselines3.common.sb2_compat.rmsprop_tf_like import RMSpropTFLike 
+from stable_baselines3.common.env_util import make_vec_env
 
 from wagner_linear_environment import WagnerLinearEnvironment
 
 if __name__ == '__main__':
-    env = WagnerLinearEnvironment(18)
+    number_of_nodes=10
+    env = WagnerLinearEnvironment(number_of_nodes)
     model = PPO("MlpPolicy", env, verbose=0)
-    model.learn(total_timesteps=10_000_000, log_interval=4)
-    model.save("WagnerLinearEnvironmentPPO10_000_000")
-    env.render_mode = "human"
+    total_timesteps = 100_000
+    model.learn(total_timesteps=total_timesteps, log_interval=10)
+    model.save(f"WagnerLinearEnvironment{number_of_nodes}PPO{total_timesteps}")
     obs, info = env.reset()
     while True:
         action, _states = model.predict(obs, deterministic=True)
         obs, reward, terminated, truncated, info = env.step(action)
         print(reward)
         if terminated or truncated:
+            env.render()
             obs, info = env.reset()
             break
